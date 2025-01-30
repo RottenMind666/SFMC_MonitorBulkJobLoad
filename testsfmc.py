57512a781518a8e2ac3907afb7a7dcb229d5651f
Santolo Toscano <santolotoscano@gmail.com>
2025-01-30 17:24:55 +0100

    fix for multiple jobs

--- testsfmc.py
+++ testsfmc.py
@@ -1,41 +1,45 @@
 import requests
 import json
 
-# Configurazione SFMC
-ID_ORG = '' #like mqfay44mzvt...
-CLIENT_ID = "" #like gqfau4n...
-CLIENT_SECRET = "" #like qqfaq46...
-AUTH_URL = f"https://{IDORG}.rest.auth.marketingcloudapis.com/v2/token"
-BASE_URL = f"https://{IDORG}.rest.marketingcloudapis.com"
-REQUEST_ID = "" #like c33e9722-28e5-4qfa24..
 
-# Ottieni token di accesso
 def get_access_token():
     payload = {
         "grant_type": "client_credentials",
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET
+        "client_id": "",
+        "client_secret": "",
     }
-    response = requests.post(AUTH_URL, json=payload)
+    url = ""  # like mc2399af9poaporlqfwch-x2ad0.
+    response = requests.post(
+        f"https://{url}.auth.marketingcloudapis.com/v2/token", json=payload
+    )
 
     print(response)
 
     response.raise_for_status()
     return response.json()["access_token"]
 
+
 # Controlla lo stato di un bulk data load
-def check_bulk_data_load_status():
+def check_bulk_data_load_status(job_id):
     access_token = get_access_token()
     print(access_token)
     headers = {
         "Authorization": f"Bearer {access_token}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
-    url = f"{BASE_URL}/data/v1/async/{REQUEST_ID}/results"
+    url = ""  # like mc2399af9poaporlqfwch-x2ad0.
+    url = f"https://{url}.rest.marketingcloudapis.com/data/v1/async/{job_id}/results"
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.json()
 
+
 if __name__ == "__main__":
-    status = check_bulk_data_load_status()
-    print(json.dumps(status, indent=4))
+    data = [
+        '{"requestId":"nqwn2367-aca7-r31qw-1242-90cdqwqwffccf","resultMessages":[]}'
+    ]
+    parsed_data = [json.loads(item)['requestId'] for item in data]
+
+    for el in parsed_data:
+        status = check_bulk_data_load_status(el)
+        print(json.dumps(status, indent=4))
